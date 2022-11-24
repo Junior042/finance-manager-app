c2b925b08674dcf024a3ccd22d465a5ee9e89e1e
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2022-11-24 16:31:29 -0300

    finalização tela register

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import "../../index.css";
 
-const Input = ({
-    registerValue,
-    objNameValueInput,
-    placeHolder
-}: any) => {
+const Input = ({ registerValue, objNameValueInput, placeHolder }: any) => {
     return (
         <>
-            <input type="text" placeholder={placeHolder} {...registerValue(objNameValueInput)}/>
+            <input
+                type="text"
+                placeholder={placeHolder}
+                {...registerValue(objNameValueInput)}
+                className="px-2 py-2 text-black rounded-md border-gray-400 border-2 bg-white px-2 placeholder:font-semibold placeholder:opacity-[.6]"
+            />
         </>
     );
-}
+};
 
 export default Input;
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import './RegisterStyle.css';
 import { Input } from '../../components';
 import { useForm } from 'react-hook-form';
+import '../../index.css';
 
 const Register = () => {
     const { register, handleSubmit } = useForm({ shouldUseNativeValidation: true });
@@ -12,14 +13,17 @@ const Register = () => {
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Input registerValue={register} objNameValueInput={'nomeUsuario'} placeHolder={"Nome Usuário"}/>
-                <Input registerValue={register} objNameValueInput={'cpf'} placeHolder={"cpf"}/>
-                <Input registerValue={register} objNameValueInput={'telefone'} placeHolder={"14999999999"}/>
-                <Input registerValue={register} objNameValueInput={'email'} placeHolder={"exemplo@gmail.com"}/>
-                <Input registerValue={register} objNameValueInput={'senha'} placeHolder={"Digite sua senha..."}/>
+            <form onSubmit={handleSubmit(onSubmit)} className="max-w-[340px] m-auto">
+                <div className="flex flex-col gap-3">
+                    <Input registerValue={register} objNameValueInput={'nomeUsuario'} placeHolder={"Nome Usuário"}/>
+                    <Input registerValue={register} objNameValueInput={'cpf'} placeHolder={"cpf"}/>
+                    <Input registerValue={register} objNameValueInput={'telefone'} placeHolder={"14999999999"}/>
+                    <Input registerValue={register} objNameValueInput={'email'} placeHolder={"exemplo@gmail.com"}/>
+                    <Input registerValue={register} objNameValueInput={'senha'} placeHolder={"Digite sua senha..."}/>
+                </div>
                 
-                <button type="submit">Enviar</button>
+                <button type="submit" className="btn my-4">Enviar</button>
+                <p className=" text-white">Já tem uma conta? <a href="#" className="text-violet-400">Faça login!</a></p>
             </form>
         </>
     );
